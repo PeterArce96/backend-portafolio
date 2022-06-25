a22a479947d7c57f25f752466cd0af6d224c2717
PeterArce96 <parce0496@gmail.com>
2022-06-24 20:54:17 -0500

    semana2-dia5

---
+++ app/FirebaseAdmin.py
@@ -0,0 +1,24 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+class FirebaseAdmin:
+
+    def __init__(self):
+        cred = credentials.Certificate("app/token.json")
+        firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
+
+    def getCollection(self,collectionName):
+        lstCollection = []
+        collectionValues = self.db.collection(collectionName)
+        docValues = collectionValues.get()
+        for doc in docValues:
+            dicCollection = doc.to_dict()
+            lstCollection.append(dicCollection)
+        
+        return lstCollection
+
+    def getDocument(self,collectionName,documentId):
+        docValue = self.db.collection(collectionName).document(documentId).get()
+        return docValue.to_dict()
